0175addd198974beace7a2abe3bd637e8651318d
Bryan Chen <xlchen1291@gmail.com>
2022-10-25 17:54:43 +1300

    chain and state tests

--- e2e/chain.test.ts
+++ e2e/chain.test.ts
@@ -1,9 +1,11 @@
+/* eslint-disable no-async-promise-executor */
+
 import { describe, expect, it } from 'vitest'
 
 import { api } from './helper'
 
 describe('chain rpc', () => {
-  it('getter works', async () => {
+  it('getXXX', async () => {
     const hashHead = '0x68cff8682eda3e5e63b375253bdb3a01f0dce1879fe7ade97c9697406c56b55a'
     const hash0 = '0x23fc729c2cdb7bd6770a4e8c58748387cc715fcf338f1f74a16833d90383f4b0'
     const hash1000 = '0x7fbf942ac7a197ed6c9ecb7733bb1d42347b7b88c32973857cc13bd98febbbab'
@@ -23,5 +25,19 @@ describe('chain rpc', () => {
     expect((await api.rpc.chain.getBlock(hash1000)).toJSON()).toMatchSnapshot()
 
     expect((await api.rpc.chain.getFinalizedHead()).toHex()).toMatch(hashHead)
+
+    // TODO: advance block and have more tests
+  })
+
+  it('subscribeNewHeads', async () => {
+    await new Promise<void>(async (resolve) => {
+      const unsub = await api.rpc.chain.subscribeNewHeads((header) => {
+        expect(header.toJSON()).toMatchSnapshot()
+        unsub()
+        resolve()
+      })
+    })
+
+    // TODO: advance block and have more tests and unsure unsubscribe works
   })
 })
