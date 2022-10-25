b75ddb3c36b8699e5dc750087685d8367605aa83
Bryan Chen <xlchen1291@gmail.com>
2022-10-25 20:46:47 +1300

    storage tests

--- e2e/storage.test.ts
+++ e2e/storage.test.ts
@@ -2,11 +2,11 @@ import { describe, it } from 'vitest'
 
 import { api, expectJson } from './helper'
 
-describe.only('storage', () => {
+describe('storage', () => {
   it('getStorage', async () => {
     await expectJson(api.query.timestamp.now()).toMatchInlineSnapshot('1666661202090')
     await expectJson(
-      api.query.tokens.accounts('5F98oWfz2r5rcRVnP9VCndg33DAAsky3iuoBSpaPUbgN9AJn', { Token: 'ACA' })
-    ).toMatchInlineSnapshot()
+      api.query.tokens.accounts('5F98oWfz2r5rcRVnP9VCndg33DAAsky3iuoBSpaPUbgN9AJn', { Token: 'AUSD' })
+    ).toMatchSnapshot()
   })
 })
