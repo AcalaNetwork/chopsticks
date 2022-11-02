1a4e747c179b9bd1dbb8b8e90f4066df7561c93c
Ermal Kaleci <ermalkaleci@gmail.com>
2022-11-02 13:08:00 +0100

    update tests

--- src/utils/import-storage.ts
+++ src/utils/import-storage.ts
@@ -6,7 +6,7 @@ import { defaultLogger } from '../logger'
 import { setStorage } from './set-storage'
 
 export const importStorage = async (storagePath: string, chain: Blockchain) => {
-  if (!existsSync(storagePath)) throw Error(`File path ${storagePath} does not exist`)
+  if (!existsSync(storagePath)) throw Error(`File ${storagePath} does not exist`)
   const storage: any = yaml.load(String(readFileSync(storagePath)))
   await setStorage(chain, storage)
   defaultLogger.trace({ storage }, 'ImportStorage')
