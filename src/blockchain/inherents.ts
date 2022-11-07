310455521df392428c37453a6c33ebe0a78bcd35
Bryan Chen <xlchen1291@gmail.com>
2022-11-07 18:11:58 +1300

    replay old set valdiation data

--- src/blockchain/inherents.ts
+++ src/blockchain/inherents.ts
@@ -1,9 +1,13 @@
 import { ApiPromise } from '@polkadot/api'
+import { TaskManager } from '../task'
 
 import { Block } from './block'
 
-export interface InherentProvider {
+export interface CreateInherents {
   createInherents(api: ApiPromise, timestamp: number, parent: Block): Promise<string[]>
+}
+
+export interface InherentProvider extends CreateInherents {
   getTimestamp(): number
 }
 
@@ -20,5 +24,65 @@ export class SetTimestamp implements InherentProvider {
 
   getTimestamp(): number {
     return this.#getTimestamp()
+  }
+}
+
+export class InherentProviders implements InherentProvider {
+  readonly #base: InherentProvider
+  readonly #providers: CreateInherents[]
+
+  constructor(base: InherentProvider, providers: CreateInherents[]) {
+    this.#base = base
+    this.#providers = providers
+  }
+
+  async createInherents(api: ApiPromise, timestamp: number, parent: Block): Promise<string[]> {
+    const base = await this.#base.createInherents(api, timestamp, parent)
+    const extra = await Promise.all(this.#providers.map((provider) => provider.createInherents(api, timestamp, parent)))
+    return [...base, ...extra.flat()]
+  }
+
+  getTimestamp(): number {
+    return this.#base.getTimestamp()
+  }
+}
+
+export class SetValidationData implements CreateInherents {
+  readonly #tasks: TaskManager
+  readonly #expectedIndex: number
+
+  constructor(tasks: TaskManager, expectedIndex: number) {
+    this.#tasks = tasks
+    this.#expectedIndex = expectedIndex
+  }
+
+  async createInherents(api: ApiPromise, _timestamp: number, parent: Block): Promise<string[]> {
+    if (!api.tx.parachainSystem?.setValidationData) {
+      return []
+    }
+    void this.#tasks // TODO
+
+    const parentBlock = await parent.parentBlock
+    if (!parentBlock) {
+      throw new Error('Parent block not found')
+    }
+    const extrinsics = await parentBlock.extrinsics
+    const method = api.createType('GenericExtrinsic', extrinsics[this.#expectedIndex])
+    const validationData = (method as any).args[0].toJSON()
+
+    const newData = {
+      ...validationData,
+      validationData: {
+        ...validationData.validationData,
+        relayParentNumber: validationData.validationData.relayParentNumber + 2,
+      },
+    }
+
+    console
+
+    const inherent = api.tx.parachainSystem.setValidationData(newData)
+    console.dir(inherent.toHuman(), { depth: null })
+
+    return [inherent.toHex()]
   }
 }
