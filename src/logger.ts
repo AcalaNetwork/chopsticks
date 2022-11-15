18b91065044223095af3cefd21441cc92e627137
Bryan Chen <xlchen1291@gmail.com>
2022-11-16 12:40:43 +1300

    improve pino-pretty usage

--- src/logger.ts
+++ src/logger.ts
@@ -1,6 +1,9 @@
 import pino from 'pino'
 export const defaultLogger = pino({
   level: process.env.LOG_LEVEL || 'info',
+  transport: {
+    target: 'pino-pretty',
+  },
 })
 
 export const truncate = (str?: string) => {
