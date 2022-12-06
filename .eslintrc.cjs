7802ee167c547925f14bb53a697133cad1ead0e1
Nantian <nxiiun@gmail.com>
2022-12-06 23:46:54 +1300

    fix lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,27 +1,27 @@
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'import', 'sort-imports-es6-autofix'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-    'plugin:import/recommended',
-    'plugin:import/typescript',
-  ],
-  rules: {
-    '@typescript-eslint/no-empty-function': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    'sort-imports-es6-autofix/sort-imports-es6': 'error',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/ban-ts-comment': 'off',
-    '@typescript-eslint/no-unused-vars': [
-      'warn',
-      {
-        argsIgnorePattern: '^_',
-        varsIgnorePattern: '^_',
-        caughtErrorsIgnorePattern: '^_',
-      },
-    ],
-  },
+	root: true,
+	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint', 'import', 'sort-imports-es6-autofix'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'prettier',
+		'plugin:import/recommended',
+		'plugin:import/typescript',
+	],
+	rules: {
+		'@typescript-eslint/no-empty-function': 'off',
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		'sort-imports-es6-autofix/sort-imports-es6': 'error',
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/ban-ts-comment': 'off',
+		'@typescript-eslint/no-unused-vars': [
+			'warn',
+			{
+				argsIgnorePattern: '^_',
+				varsIgnorePattern: '^_',
+				caughtErrorsIgnorePattern: '^_',
+			},
+		],
+	},
 }
