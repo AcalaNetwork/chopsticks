799cde60d4aa29e9bd211198f18343d26240ba5e
Ermal Kaleci <ermalkaleci@gmail.com>
2022-12-18 12:16:21 +0100

    use namespaced binding

--- executor/src/bindings.rs
+++ executor/src/bindings.rs
@@ -2,12 +2,12 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 extern "C" {
-    #[wasm_bindgen(js_name = "getStorage")]
+    #[wasm_bindgen(js_namespace = _chopsticks_binding_, js_name = "getStorage")]
     pub async fn get_storage(hash: JsValue, key: JsValue) -> JsValue;
 
-    #[wasm_bindgen(js_name = "getPrefixKeys")]
+    #[wasm_bindgen(js_namespace = _chopsticks_binding_, js_name = "getPrefixKeys")]
     pub async fn get_prefix_keys(hash: JsValue, key: JsValue) -> JsValue;
 
-    #[wasm_bindgen(js_name = "getNextKey")]
+    #[wasm_bindgen(js_namespace = _chopsticks_binding_, js_name = "getNextKey")]
     pub async fn get_next_key(hash: JsValue, key: JsValue) -> JsValue;
 }
--- src/bindings.ts
+++ src/bindings.ts
@@ -6,24 +6,24 @@ import { defaultLogger, truncate } from './logger'
 const logger = defaultLogger.child({ name: 'binding' })
 
 export const setupBindings = (chain: Blockchain) => {
-  global.getStorage = async function (blockHash: HexString, key: HexString) {
-    const block = await chain.getBlock(blockHash)
-    if (!block) throw Error(`Block not found ${blockHash}`)
-    const value = await block.get(key).then((x) => (x ? x : '0x'))
-    logger.trace({ blockHash, key, value: truncate(value) }, 'exec_storageGet')
-    return value
-  }
-
-  global.getPrefixKeys = async function (blockHash: HexString, key: HexString) {
-    const block = await chain.getBlock(blockHash)
-    if (!block) throw Error(`Block not found ${blockHash}`)
-    return block.getKeysPaged({ prefix: key, pageSize: 1000, startKey: key })
-  }
-
-  global.getNextKey = async function (blockHash: HexString, key: HexString) {
-    const block = await chain.getBlock(blockHash)
-    if (!block) throw Error(`Block not found ${blockHash}`)
-    const keys = await block.getKeysPaged({ prefix: key, pageSize: 1, startKey: key })
-    return keys.length > 0 ? keys[0] : '0x'
+  global._chopsticks_binding_ = {
+      getStorage: async function (blockHash: HexString, key: HexString) {
+      const block = await chain.getBlock(blockHash)
+      if (!block) throw Error(`Block not found ${blockHash}`)
+      const value = await block.get(key).then((x) => (x ? x : '0x'))
+      logger.trace({ blockHash, key, value: truncate(value) }, 'exec_storageGet')
+      return value
+    },
+    getPrefixKeys:  async function (blockHash: HexString, key: HexString) {
+      const block = await chain.getBlock(blockHash)
+      if (!block) throw Error(`Block not found ${blockHash}`)
+      return block.getKeysPaged({ prefix: key, pageSize: 1000, startKey: key })
+    },
+    getNextKey: async function (blockHash: HexString, key: HexString) {
+      const block = await chain.getBlock(blockHash)
+      if (!block) throw Error(`Block not found ${blockHash}`)
+      const keys = await block.getKeysPaged({ prefix: key, pageSize: 1, startKey: key })
+      return keys.length > 0 ? keys[0] : '0x'
+    }
   }
 }
