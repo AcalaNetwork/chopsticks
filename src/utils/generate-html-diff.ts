fcbc46daa44590bbfc53a8f3351fd94862739499
Ermal Kaleci <ermalkaleci@gmail.com>
2023-01-22 22:10:49 +0100

    fix bundle to include template

--- src/utils/generate-html-diff.ts
+++ src/utils/generate-html-diff.ts
@@ -3,10 +3,11 @@ import { HexString } from '@polkadot/util/types'
 import { decodeStorageDiff } from './decoder'
 import { mkdirSync, readFileSync, writeFileSync } from 'node:fs'
 import { template } from 'lodash'
+import path from 'node:path'
 
 export const generateHtmlDiff = async (block: Block, diff: [HexString, HexString | null][]) => {
   const [left, _right, delta] = await decodeStorageDiff(block, diff)
-  const htmlTemplate = readFileSync('./template/diff.html', 'utf-8')
+  const htmlTemplate = readFileSync(path.join(__dirname, '../../template/diff.html'), 'utf-8')
   return template(htmlTemplate)({ left: JSON.stringify(left), delta: JSON.stringify(delta) })
 }
 
