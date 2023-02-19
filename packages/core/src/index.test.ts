1e02187a1ca71306a68262f26f6b20cd461bc308
Nantian <nxiiun@gmail.com>
2023-02-20 12:08:27 +1300

    add core

---
+++ packages/core/src/index.test.ts
@@ -0,0 +1,6 @@
+import { expect, it } from 'vitest'
+import core from './index'
+
+it('@acala-network/chopsticks-core', () => {
+  expect(core).toBe('@acala-network/chopsticks-core')
+})
---
+++ packages/core/src/index.ts
@@ -0,0 +1,1 @@
+export default '@acala-network/chopsticks-core'
