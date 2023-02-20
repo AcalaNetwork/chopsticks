54673c1b90a8d3480383e3af50b9610a2f5e1fd5
Nantian <nxiiun@gmail.com>
2023-02-20 16:43:39 +1300

    update port

--- e2e/scripts/sidecar-chopsticks.sh
+++ e2e/scripts/sidecar-chopsticks.sh
@@ -6,14 +6,14 @@ set -xe
 git clone https://github.com/paritytech/substrate-api-sidecar.git
 
 # start chopsticks node
-yarn start dev --endpoint wss://rpc.polkadot.io --port 8000 & POLKADOT_PID=$!
-yarn start dev --endpoint wss://statemint-rpc.polkadot.io --port 8001 & STATEMINT_PID=$!
+yarn start dev --endpoint wss://rpc.polkadot.io --port 8011 & POLKADOT_PID=$!
+yarn start dev --endpoint wss://statemint-rpc.polkadot.io --port 8012 & STATEMINT_PID=$!
 
 # run tests
 (cd substrate-api-sidecar && \
 yarn && \
-yarn test:latest-e2e-tests --local ws://localhost:8000 --chain polkadot && \
-yarn test:latest-e2e-tests --local ws://localhost:8001 --chain statemint)
+yarn test:latest-e2e-tests --local ws://localhost:8011 --chain polkadot && \
+yarn test:latest-e2e-tests --local ws://localhost:8012 --chain statemint)
 
 #cleanup
 rm -rf substrate-api-sidecar
