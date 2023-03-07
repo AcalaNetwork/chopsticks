e4621f2c8cea95f299ca21585f49898e7c1194d1
Bryan Chen <xlchen1291@gmail.com>
2023-03-07 17:14:47 +1300

    fix

--- e2e/src/block.test.ts
+++ e2e/src/block.test.ts
@@ -1,19 +1,28 @@
-import { describe, expect, it } from 'vitest'
+import { afterAll, describe, expect, it } from 'vitest'
 
-import { chain, delay, dev, setupApi } from './helper'
+import { delay } from './helper'
+import networks from './networks'
 
-describe('block', () => {
-  setupApi({ endpoint: 'wss://rpc.polkadot.io' })
+describe('block', async () => {
+  const acala = await networks.acala()
+  const { chain, dev } = acala
+
+  afterAll(async () => {
+    await acala.teardown()
+  })
 
   it('upcoming block works', async () => {
+    const promises: Promise<any>[] = []
     expect(await chain.upcomingBlocks()).toEqual(0)
-    dev.newBlock()
+    promises.push(dev.newBlock())
     await delay(10)
     expect(await chain.upcomingBlocks()).toEqual(1)
 
-    dev.newBlock()
-    dev.newBlock()
+    promises.push(dev.newBlock())
+    promises.push(dev.newBlock())
     await delay(10)
     expect(await chain.upcomingBlocks()).toEqual(2)
+
+    await Promise.all(promises)
   })
 })
