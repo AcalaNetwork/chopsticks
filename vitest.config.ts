ee5bb30af78bfb80d565fe55e4721c379242fe3b
Ermal Kaleci <ermalkaleci@gmail.com>
2023-03-08 16:49:11 +0100

    setup fixes (#230)

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vitest/config'
 import swc from 'unplugin-swc'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   test: {
     hookTimeout: 30000,
     testTimeout: 120000,
   },
-  plugins: [swc.vite()],
+  plugins: [swc.vite(), tsconfigPaths()],
 })
