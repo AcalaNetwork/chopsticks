92b71d2ca1236d12d9321e5a81221a8edf34d727
Bryan Chen <xlchen1291@gmail.com>
2023-03-09 21:33:12 +1300

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,7 +26,7 @@ module.exports = {
   settings: {
     'import/resolver': {
       typescript: {
-        project: 'packages/*/tsconfig.json',
+        project: 'tsconfig.json',
       },
     },
   },
