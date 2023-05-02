b698cd4f9124e76b8d76bf9ec52e35c2db0caaf3
Ermal Kaleci <ermalkaleci@gmail.com>
2023-05-02 16:21:05 +0200

    fix block-build test and update actions (#287)

--- packages/e2e/src/build-block.test.ts
+++ packages/e2e/src/build-block.test.ts
@@ -2,28 +2,39 @@ import { afterAll, describe, expect, it } from 'vitest'
 
 import { setupAll } from './helper'
 
+const KUSAMA_STORAGE = {
+  FellowshipCollective: {
+    $removePrefix: ['IdToIndex', 'IndexToId', 'MemberCount', 'Members'],
+    Voting: [],
+  },
+  ParasDisputes: {
+    $removePrefix: ['disputes'],
+  },
+}
+
 describe.each([
   { chain: 'Polkadot', endpoint: 'wss://rpc.polkadot.io' },
   { chain: 'Statemint', endpoint: 'wss://statemint-rpc.polkadot.io' },
   { chain: 'Polkadot Collectives', endpoint: 'wss://polkadot-collectives-rpc.polkadot.io' },
   { chain: 'Acala', endpoint: 'wss://acala-rpc-1.aca-api.network' },
 
-  { chain: 'Kusama', endpoint: 'wss://kusama-rpc.polkadot.io' },
+  { chain: 'Kusama', endpoint: 'wss://kusama-rpc.polkadot.io', storage: KUSAMA_STORAGE },
   { chain: 'Statemine', endpoint: 'wss://statemine-rpc.polkadot.io' },
   { chain: 'Karura', endpoint: 'wss://karura-rpc-1.aca-api.network' },
 
   { chain: 'Westend', endpoint: 'wss://westend-rpc.polkadot.io' },
   { chain: 'Westmint', endpoint: 'wss://westmint-rpc.polkadot.io' },
   { chain: 'Westend Collectives', endpoint: 'wss://sys.ibp.network/collectives-westend' },
-])('Latest $chain can build blocks', async ({ endpoint }) => {
+])('Latest $chain can build blocks', async ({ endpoint, storage }) => {
   const { setup, teardownAll } = await setupAll({ endpoint })
 
   afterAll(async () => {
     await teardownAll()
   })
 
-  it.skipIf(process.env.BUILD_BLOCK_TEST !== 'true')('build blocks', async () => {
+  it.runIf(process.env.CI)('build blocks', async () => {
     const { chain, ws, teardown } = await setup()
+    storage && (await ws.send('dev_setStorage', [storage]))
     const blockNumber = chain.head.number
     await ws.send('dev_newBlock', [{ count: 2 }])
     expect(chain.head.number).eq(blockNumber + 2)
