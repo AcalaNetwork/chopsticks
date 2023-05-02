7379a7bdb1a8b1fa8d5347f003b0d9a65fd38d4f
Ermal Kaleci <ermalkaleci@gmail.com>
2023-05-02 15:35:52 +0200

    test: failed transaction should revert changes properly

---
+++ packages/e2e/src/failed-tx-reverts.test.ts
@@ -0,0 +1,36 @@
+import { afterAll, describe, expect, it } from 'vitest'
+import { testingPairs } from './helper'
+import networks from './networks'
+
+// SPECIAL TEST CASE. DO NOT CHANGE
+describe('failed tx should revert storage changes', async () => {
+  const { alice } = testingPairs()
+  const { api, dev, teardown } = await networks.acala({ blockNumber: 3478171 })
+
+  await dev.setStorage({
+    System: {
+      Account: [[[alice.address], { data: { free: 1000 * 1e12 } }]],
+    },
+    Tokens: {
+      Accounts: [
+        [[alice.address, { Token: 'AUSD' }], { free: 1000000000000000 }],
+        [[alice.address, { Token: 'DOT' }], { free: 1000000000000000 }],
+        [[alice.address, { Token: 'LDOT' }], { free: 1000000000000000 }],
+      ],
+    },
+    Homa: { $removePrefix: ['redeemRequests', 'unbondings', 'toBondPool'] },
+  })
+
+  afterAll(async () => {
+    await teardown()
+  })
+
+  it('works', async () => {
+    await api.tx.honzon.adjustLoan({ Token: 'DOT' }, 1000000000000, 1000000000000000).signAndSend(alice)
+    await dev.newBlock()
+    await api.tx.honzon.closeLoanHasDebitByDex({ Token: 'DOT' }, 1000000000000).signAndSend(alice)
+    await dev.newBlock()
+    const events = await api.query.system.events()
+    expect(events.toHuman()).toMatchSnapshot()
+  })
+})
