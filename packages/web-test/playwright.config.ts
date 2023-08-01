609660a18c4fcb96f87f81fc0d943995e4afabe2
Ermal Kaleci <ermalkaleci@gmail.com>
2023-08-01 13:37:00 +0200

    update tests

--- packages/web-test/playwright.config.ts
+++ packages/web-test/playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: 1,
+  workers: process.env.CI ? 1 : 4,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -69,9 +69,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run vite:serve',
+    url: 'http://127.0.0.1:3000',
+    reuseExistingServer: !process.env.CI,
+  },
 })
--- packages/web-test/tests/index.spec.ts
+++ packages/web-test/tests/index.spec.ts
@@ -1,29 +1,9 @@
-import { ChildProcessWithoutNullStreams, spawn } from 'node:child_process'
 import { expect, test } from '@playwright/test'
 
-const startServer = async (): Promise<ChildProcessWithoutNullStreams> => {
-  console.log('Starting server...')
-  return new Promise((resolve, reject) => {
-    const server = spawn('yarn', ['vite:serve'])
-    server.stderr.on('data', () => {
-      reject()
-    })
-    server.stdout.on('data', (data) => {
-      if (data.toString().includes('Local')) {
-        console.log('Server started')
-        resolve(server)
-      }
-    })
-  })
-}
-
 test('build blocks successfully', async ({ page }) => {
-  const process = await startServer()
-  console.log('Running tests...')
+  test.slow()
 
   await page.goto('http://localhost:3000')
-
-  test.slow()
 
   // starts with Loading...
   await expect(page.locator('div#app')).toHaveText(/Loading.../)
@@ -33,6 +13,4 @@ test('build blocks successfully', async ({ page }) => {
 
   // wait for new block
   await expect(page.locator('div#app')).toHaveText(/4000001/, { timeout: 120_000 })
-
-  process.kill()
 })
