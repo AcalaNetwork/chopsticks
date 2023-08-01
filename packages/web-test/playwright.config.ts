ac0ae578bb63ec45aa85fa6a4239112366956fe0
Ermal Kaleci <ermalkaleci@gmail.com>
2023-08-01 15:29:45 +0200

    debug

--- packages/web-test/playwright.config.ts
+++ packages/web-test/playwright.config.ts
@@ -16,16 +16,6 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
   ],
 
   webServer: {
