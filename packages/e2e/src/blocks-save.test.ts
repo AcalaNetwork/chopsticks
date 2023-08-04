4b74f0b2af235a44bc58eb4e36c56dadfa6b0260
Qiwei Yang <yangqiwei97@gmail.com>
2023-08-04 09:08:04 +0800

    remove optional chaining in test

--- packages/e2e/src/blocks-save.test.ts
+++ packages/e2e/src/blocks-save.test.ts
@@ -13,15 +13,15 @@ describe('block-save', async () => {
   it('saved blocks data', async () => {
     await dev.newBlock({ count: 2 })
 
-    const numberOfBlocks = await chain.db?.getRepository('Block').count()
+    const numberOfBlocks = await chain.db!.getRepository('Block').count()
     expect(numberOfBlocks).toEqual(2)
 
     const block = await chain.getBlockAt(chain.head.number)
-    const blockData = await chain.db?.getRepository('Block').findOne({ where: { number: chain.head.number } })
-    expect(blockData?.hash).toEqual(block?.hash)
-    expect(JSON.stringify(blockData?.header)).toEqual(JSON.stringify(block?.header))
-    expect(blockData?.parentHash).toEqual((await block?.parentBlock)?.hash)
-    expect(JSON.stringify(blockData?.extrinsics)).toEqual(JSON.stringify(await block?.extrinsics))
-    expect(JSON.stringify(blockData?.storageDiff)).toEqual(JSON.stringify(await block?.storageDiff()))
+    const blockData = await chain.db!.getRepository('Block').findOne({ where: { number: chain.head.number } })
+    expect(blockData!.hash).toEqual(block!.hash)
+    expect(JSON.stringify(blockData!.header)).toEqual(JSON.stringify(block!.header))
+    expect(blockData!.parentHash).toEqual((await block!.parentBlock)!.hash)
+    expect(JSON.stringify(blockData!.extrinsics)).toEqual(JSON.stringify(await block!.extrinsics))
+    expect(JSON.stringify(blockData!.storageDiff)).toEqual(JSON.stringify(await block!.storageDiff()))
   })
 })
