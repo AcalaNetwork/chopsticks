af673ff5873ea92976b9b7cce8d919f3b30fd71c
Ermal Kaleci <ermalkaleci@gmail.com>
2023-08-09 12:27:31 +0200

    fix module (#362)

--- packages/core/src/db/browser.ts
+++ packages/core/src/db/browser.ts
@@ -2,11 +2,14 @@ import { DataSource } from 'typeorm'
 import { createInstance } from 'localforage'
 import initSqlJs from 'sql.js'
 
-globalThis.localforage = createInstance({ name: 'chopsticks' })
-
 import * as entities from './entities'
 
 export const openDb = async (dbPath: string): Promise<DataSource> => {
+  if (!globalThis.localforage) {
+    globalThis.localforage = createInstance({ name: 'chopsticks' })
+  }
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   const wasmUrl = new URL('../../../../node_modules/sql.js/dist/sql-wasm.wasm', import.meta.url)
   const wasmBinary = await fetch(wasmUrl).then((response) => response.arrayBuffer())
   const source = new DataSource({
