727de6b1818d138c903e0085fc6c817aea3ec577
shunjizhan <shunhahaha@live.com>
2023-08-24 12:25:49 +0800

    export sign fake

---
+++ packages/chopsticks/src/utils/signFake.ts
@@ -0,0 +1,37 @@
+import { ApiPromise } from "@polkadot/api";
+import { GenericExtrinsic } from "@polkadot/types"
+import { SignatureOptions } from "@polkadot/types/types"
+
+export type signFakeOptions = Partial<SignatureOptions>;
+
+export const signFakeWithApi = async (
+  api: ApiPromise,
+  tx: GenericExtrinsic,
+  addr: string,
+  options: signFakeOptions = {},
+) => {
+  const nonce = options.nonce ?? (await api.query.system.account(addr)).nonce;
+
+  return signFake(tx, addr, {
+    nonce,
+    genesisHash: api.genesisHash,
+    runtimeVersion: api.runtimeVersion,
+    blockHash: api.genesisHash,
+    ...options,
+  });
+}
+
+export const signFake = (
+  tx: GenericExtrinsic,
+  addr: string,
+  options: SignatureOptions,
+) => {
+  const mockSignature = new Uint8Array(64);
+  mockSignature.fill(0xcd);
+  mockSignature.set([0xde, 0xad, 0xbe, 0xef]);
+  tx.signFake(addr, options);
+
+  tx.signature.set(mockSignature);
+
+  return tx;
+}
