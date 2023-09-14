7fe138e28562e304b30d9cf51d6781b9b7e7a46a
Qiwei Yang <yangqiwei97@gmail.com>
2023-09-14 11:10:04 +0800

    remove comments

--- packages/web-test/vite.config.js
+++ packages/web-test/vite.config.js
@@ -4,12 +4,4 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   plugins: [tsconfigPaths()],
   base: '/chopsticks/',
-  // esbuild: {
-  //   keepNames: true, // typeorm migrations require timestamp in their names and this way it can be defined with name property
-  // },
-  // build: {
-  //   commonjsOptions: {
-  //     transformMixedEsModules: true, // https://github.com/rollup/plugins/tree/master/packages/commonjs#transformmixedesmodules
-  //   },
-  // },
 })
