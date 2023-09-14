441364186d8e7d49a242a463da5646efa84de5f7
Qiwei Yang <yangqiwei97@gmail.com>
2023-09-14 10:52:17 +0800

    fix typeorm vite bundle using a patch

--- packages/web-test/vite.config.js
+++ packages/web-test/vite.config.js
@@ -4,4 +4,12 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   plugins: [tsconfigPaths()],
   base: '/chopsticks/',
+  // esbuild: {
+  //   keepNames: true, // typeorm migrations require timestamp in their names and this way it can be defined with name property
+  // },
+  // build: {
+  //   commonjsOptions: {
+  //     transformMixedEsModules: true, // https://github.com/rollup/plugins/tree/master/packages/commonjs#transformmixedesmodules
+  //   },
+  // },
 })
