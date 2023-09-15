748900ab21b66a19c9cd2c176c9419c688cf7615
Qiwei Yang <yangqiwei97@gmail.com>
2023-09-15 14:47:00 +0800

    refactor api-extractor config

--- packages/web-test/vite.config.js
+++ packages/web-test/vite.config.js
@@ -4,4 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   plugins: [tsconfigPaths()],
   base: '/chopsticks/',
+  build: {
+    outDir: '../../dist',
+  },
 })
