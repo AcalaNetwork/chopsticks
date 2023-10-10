9efe24b3f86bb58bee7ebe682002ecbea469f2c5
Qiwei Yang <yangqiwei97@gmail.com>
2023-10-10 14:47:51 +0800

    add chopsticks provider

---
+++ packages/core/src/chopsticks-provider.ts
@@ -0,0 +1,185 @@
+import { EventEmitter } from 'eventemitter3'
+import {
+  ProviderInterface,
+  ProviderInterfaceCallback,
+  ProviderInterfaceEmitCb,
+  ProviderInterfaceEmitted,
+  ProviderStats,
+} from '@polkadot/rpc-provider/types'
+import { truncate } from 'lodash'
+
+import { Blockchain } from './blockchain'
+import { allHandlers } from './rpc'
+import { defaultLogger } from './logger'
+import { setStorage } from './utils'
+import { setup } from './setup'
+
+interface SubscriptionHandler {
+  callback: ProviderInterfaceCallback
+  type: string
+}
+
+interface Subscription extends SubscriptionHandler {
+  method: string
+  params: unknown[]
+  onCancel?: () => void
+}
+
+export class ChopsticksProvider implements ProviderInterface {
+  #isConnected = false
+  #eventemitter: EventEmitter
+  #isReadyPromise: Promise<void>
+  #chainPromise: Promise<Blockchain>
+  #chain: Blockchain | undefined
+  #endpoint: string
+  readonly stats?: ProviderStats
+  #subscriptions: Record<string, Subscription> = {}
+
+  constructor(endpoint: string) {
+    this.#endpoint = endpoint
+    // FIXME: WARNING in /node_modules/typeorm/browser/driver/react-native/ReactNativeDriver.js
+    // see: https://github.com/typeorm/typeorm/issues/2158
+    // this repo may not have this problem since using vite, but polkadot.js app will have
+    this.#chainPromise = setup({
+      endpoint: endpoint,
+      mockSignatureHost: true,
+      db: 'chopsticks',
+    })
+
+    this.#eventemitter = new EventEmitter()
+
+    this.#isReadyPromise = new Promise((resolve, reject): void => {
+      this.#eventemitter.once('connected', (): void => {
+        resolve()
+      })
+      this.#eventemitter.once('error', reject)
+    })
+  }
+
+  get hasSubscriptions(): boolean {
+    return true
+  }
+
+  get isClonable(): boolean {
+    return true
+  }
+
+  get isConnected(): boolean {
+    return this.#isConnected
+  }
+
+  get isReady(): Promise<void> {
+    this.connect()
+    return this.#isReadyPromise
+  }
+
+  clone = (): ProviderInterface => {
+    return new ChopsticksProvider(this.#endpoint)
+  }
+
+  connect = async (): Promise<void> => {
+    this.#chain = await this.#chainPromise
+    await setStorage(this.#chain, {
+      System: {
+        Account: [
+          [
+            ['5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY'],
+            {
+              providers: 1,
+              data: {
+                free: '1000000000000000000',
+              },
+            },
+          ],
+        ],
+      },
+    })
+    this.#isConnected = true
+    this.#eventemitter.emit('connected')
+  }
+
+  disconnect = async (): Promise<void> => {
+    await this.#chain?.api?.disconnect()
+    await this.#chain?.close()
+    this.#isConnected = false
+    this.#eventemitter.emit('disconnected')
+  }
+
+  on = (type: ProviderInterfaceEmitted, sub: ProviderInterfaceEmitCb): (() => void) => {
+    this.#eventemitter.on(type, sub)
+
+    return (): void => {
+      this.#eventemitter.removeListener(type, sub)
+    }
+  }
+
+  #subscriptionManager = {
+    subscribe: (method: string, subid: string, onCancel: () => void = () => {}) => {
+      this.#subscriptions[subid].onCancel = onCancel
+      return (data: any) => {
+        if (this.#subscriptions[subid]) {
+          defaultLogger.trace({ method, subid, data: truncate(data) }, 'Subscription notification')
+          // here directly call the callback user pass in (since no network communication at all)
+          this.#subscriptions[subid].callback(null, data)
+        }
+      }
+    },
+    unsubscribe: (subid: string) => {
+      if (this.#subscriptions[subid]) {
+        this.#subscriptions[subid].onCancel?.()
+        delete this.#subscriptions[subid]
+      }
+    },
+  }
+
+  send = async <T>(
+    method: string,
+    params: unknown[],
+    _isCacheable?: boolean,
+    subscription?: SubscriptionHandler,
+  ): Promise<T> => {
+    await this.isReady
+    const handler = allHandlers[method]
+    if (!handler) {
+      defaultLogger.error(() => `Unable to find handler=${method}`)
+      return Promise.reject(new Error(`Unable to find handler=${method}`))
+    }
+    if (subscription) {
+      const subid = `${subscription.type}::${method}`
+      this.#subscriptions[subid] = {
+        callback: subscription.callback,
+        method,
+        params,
+        type: subscription.type,
+      }
+    }
+    const result = await handler({ chain: this.#chain! }, params, this.#subscriptionManager)
+    return result
+  }
+
+  subscribe(
+    type: string,
+    method: string,
+    params: unknown[],
+    callback: ProviderInterfaceCallback,
+  ): Promise<number | string> {
+    return this.send<string | number>(method, params, false, { callback, type })
+  }
+
+  async unsubscribe(type: string, method: string, id: number | string): Promise<boolean> {
+    const subscription = `${type}::${id}`
+
+    if (!this.#subscriptions[subscription]) {
+      defaultLogger.debug(() => `Unable to find active subscription=${subscription}`)
+      return false
+    }
+
+    delete this.#subscriptions[subscription]
+
+    try {
+      return this.isConnected && this.#chain ? this.send<boolean>(method, [id]) : true
+    } catch {
+      return false
+    }
+  }
+}
