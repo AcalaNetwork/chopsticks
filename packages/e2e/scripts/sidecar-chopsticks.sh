5f4f9211161fc126382e5dba23ebbf520c501c1c
Ermal Kaleci <ermalkaleci@gmail.com>
2023-10-13 22:00:48 +0200

    update sidecar

--- packages/e2e/scripts/sidecar-chopsticks.sh
+++ packages/e2e/scripts/sidecar-chopsticks.sh
@@ -2,30 +2,35 @@
 
 set -x
 
-# clone sidecard
-git clone https://github.com/paritytech/substrate-api-sidecar.git
-
 # run chopsticks node
 yarn dev:acala --port 8011 & ACALA_PID=$!
 yarn dev:karura --port 8012 & KARURA_PID=$!
 
+printf "Waiting for chains to be ready"
+until nc -z localhost 8011 && nc -z localhost 8012; do
+  sleep 5
+done
+
+SAS_SUBSTRATE_URL=ws://127.0.0.1:8011 SAS_EXPRESS_PORT=8111 npx --yes @substrate/api-sidecar & ACALA_SIDECAR_PID=$!
+SAS_SUBSTRATE_URL=ws://127.0.0.1:8012 SAS_EXPRESS_PORT=8112 npx --yes @substrate/api-sidecar & KARURA_SIDECAR_PID=$!
+
+printf "Waiting for sidecars to be ready"
+until nc -z localhost 8111 && nc -z localhost 8112; do
+  sleep 5
+done
+
+# clone sidecard
+git clone --depth 1 --branch v17.2.0 --single-branch https://github.com/paritytech/substrate-api-sidecar.git
+
 # prepare sidecar
 cd substrate-api-sidecar
-
-yarn
-yarn add -D ts-node
-
-SAS_SUBSTRATE_URL=ws://127.0.0.1:8011 SAS_EXPRESS_PORT=8111 yarn ts-node src/main.ts & ACALA_SIDECAR_PID=$!
-SAS_SUBSTRATE_URL=ws://127.0.0.1:8012 SAS_EXPRESS_PORT=8112 yarn ts-node src/main.ts & KARURA_SIDECAR_PID=$!
-
-# wait a bit for it to be ready
-sleep 10
+yarn install
 
 # run the tests
-yarn ts-node e2e-tests/latest/index.ts --chain acala --url http://127.0.0.1:8111
+npx --yes ts-node e2e-tests/latest/index.ts --chain acala --url http://127.0.0.1:8111
 ACALA_TEST_RESULT=$?
 
-yarn ts-node e2e-tests/latest/index.ts --chain karura --url http://127.0.0.1:8112
+npx --yes ts-node e2e-tests/latest/index.ts --chain karura --url http://127.0.0.1:8112
 KARURA_TEST_RESULT=$?
 
 cd ..
