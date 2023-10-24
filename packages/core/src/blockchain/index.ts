a7a3cbc2466bc8b9741283b516e6240669757f63
Qiwei Yang <yangqiwei97@gmail.com>
2023-10-24 09:09:58 +0800

    reduce default maxMemoryBlockCount (#465)

--- packages/core/src/blockchain/index.ts
+++ packages/core/src/blockchain/index.ts
@@ -110,7 +110,7 @@ export class Blockchain {
     runtimeLogLevel = 0,
     registeredTypes = {},
     offchainWorker = false,
-    maxMemoryBlockCount = 2000,
+    maxMemoryBlockCount = 500,
   }: Options) {
     this.api = api
     this.db = db
