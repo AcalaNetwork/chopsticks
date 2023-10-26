92c87a814a8fde638df52fa9a8bf37acd91a431c
Ermal Kaleci <ermalkaleci@gmail.com>
2023-10-26 21:25:19 +0200

    sqlite PRAGMA busy_timeout

--- packages/db/src/db/index.ts
+++ packages/db/src/db/index.ts
@@ -9,8 +9,9 @@ export const openDb = async (dbPath: string): Promise<DataSource> => {
     entities: Object.values(entities),
     synchronize: true,
     logging: false,
-    enableWAL: true,
-    busyErrorRetry: 1000,
+    enableWAL: true, // improve performance and concurrency
+    busyErrorRetry: 1000, // typeorm retry timeout
+    busyTimeout: 5000, // retry for 5 seconds, sqlite PRAGMA busy_timeout
   })
 
   await source.initialize()
