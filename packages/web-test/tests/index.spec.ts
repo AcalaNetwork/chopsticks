5ddb0c1e1a8b99f6e7cdbd8d2849c6642aadb2b6
Qiwei Yang <yangqiwei97@gmail.com>
2023-10-27 13:49:39 +0800

    add test

--- packages/web-test/tests/index.spec.ts
+++ packages/web-test/tests/index.spec.ts
@@ -11,7 +11,7 @@ test.describe('index', () => {
     await page.waitForLoadState()
   })
 
-  test('build blocks successfully', async ({ page }) => {
+  test.only('build blocks successfully', async ({ page }) => {
     test.setTimeout(5 * 60 * 1000) // 5 minutes timeout
     // starts with Loading...
     await expect(page.getByText('Save')).toBeDisabled()
@@ -20,6 +20,21 @@ test.describe('index', () => {
     await page.getByText(/build block/i).click()
     // wait for new block
     await expect(page.locator('#blocks-section')).toHaveText(/4000001/, { timeout: 200_000 })
+    // check indexedDB
+    const savedHash = await page.evaluate(async () => {
+      const db = window.indexedDB.open('cache', 1)
+      await new Promise((resolve) => {
+        db.onsuccess = resolve
+      })
+      const tx = db.result.transaction('block', 'readonly')
+      const store = tx.objectStore('block')
+      const block = store.index('byNumber').get(4000001)
+      await new Promise((resolve) => {
+        block.onsuccess = resolve
+      })
+      return block.result.hash
+    })
+    expect(savedHash).toBe('0x6b81a9a7fabbe32c1e685b944c8f1afd06be7e58ae48bb8d5ac50cc761d9bb77')
   })
 
   test('dry run extrinsic', async ({ page }) => {
