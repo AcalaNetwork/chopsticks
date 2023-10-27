8f0366399124350f73f7ce97da608988c908aab8
Qiwei Yang <yangqiwei97@gmail.com>
2023-10-27 12:18:48 +0800

    add idb for browser

--- packages/db/src/browser.ts
+++ packages/db/src/browser.ts
@@ -1,13 +1,84 @@
-import { DataSource } from 'typeorm'
+import { BlockEntry, Database, KeyValueEntry } from '@acala-network/chopsticks-core'
+import { DBSchema, IDBPDatabase, openDB } from 'idb'
 
-import { BaseSqlDatabase } from './base-sql'
-import { openDb } from './db/browser'
-
-export class SqljsDatabase extends BaseSqlDatabase {
-  datasource: Promise<DataSource>
+interface Schema extends DBSchema {
+  keyValue: {
+    key: string
+    value: KeyValueEntry
+  }
+  block: {
+    key: string
+    value: BlockEntry
+    indexes: { byNumber: number }
+  }
+}
+export class IdbDatabase implements Database {
+  datasource: Promise<IDBPDatabase<Schema>>
 
   constructor(location: string) {
-    super()
-    this.datasource = openDb(location)
+    this.datasource = openDB<Schema>(location, 1, {
+      upgrade(db) {
+        db.createObjectStore('keyValue', { keyPath: 'key' })
+        const blockStore = db.createObjectStore('block', { keyPath: 'hash' })
+        blockStore.createIndex('byNumber', 'number')
+      },
+    })
+  }
+
+  async close(): Promise<void> {
+    const db = await this.datasource
+    db.close()
+  }
+
+  async saveBlock(block: BlockEntry): Promise<void> {
+    const db = await this.datasource
+    const tx = db.transaction(['block'], 'readwrite')
+    const store = tx.objectStore('block')
+    store.delete(block.hash)
+    store.put(block)
+    await tx.done
+  }
+
+  async queryBlock(hash: `0x${string}`): Promise<BlockEntry | null> {
+    const db = await this.datasource
+    const block = await db.get('block', hash)
+    return block ?? null
+  }
+
+  async queryBlockByNumber(number: number): Promise<BlockEntry | null> {
+    const db = await this.datasource
+    const block = await db.getFromIndex('block', 'byNumber', number)
+    return block ?? null
+  }
+
+  async queryHighestBlock(): Promise<BlockEntry | null> {
+    const db = await this.datasource
+    const block = await db.getAllFromIndex('block', 'byNumber')
+    return block[0] ?? null
+  }
+
+  async deleteBlock(hash: `0x${string}`): Promise<void> {
+    const db = await this.datasource
+    await db.delete('block', hash)
+  }
+
+  async blocksCount(): Promise<number> {
+    const db = await this.datasource
+    return db.count('block')
+  }
+
+  async saveStorage(blockHash: `0x${string}`, key: `0x${string}`, value: `0x${string}` | null): Promise<void> {
+    const db = await this.datasource
+    await db.put('keyValue', {
+      blockHash,
+      key,
+      value,
+    })
+  }
+
+  async queryStorage(_blockHash: `0x${string}`, key: `0x${string}`): Promise<KeyValueEntry | null> {
+    const db = await this.datasource
+    const keyValue = await db.get('keyValue', key)
+    return keyValue ?? null
   }
 }
