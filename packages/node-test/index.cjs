e59ed08b4be7d825b35f79fc74af2a2196fde145
Ermal Kaleci <ermalkaleci@gmail.com>
2023-11-07 17:29:14 +0100

    update node test

--- packages/node-test/index.cjs
+++ packages/node-test/index.cjs
@@ -2,6 +2,7 @@ const { setupWithServer } = require('@acala-network/chopsticks')
 
 async function main() {
 	const server = await setupWithServer({ endpoint: 'wss://rpc.polkadot.io', db: 'db.sqlite' })
+	await server.chain.newBlock()
 	await server.close()
 }
 main()
--- packages/node-test/index.mjs
+++ packages/node-test/index.mjs
@@ -1,4 +1,5 @@
 import { setupWithServer } from '@acala-network/chopsticks'
 
 const server = await setupWithServer({ endpoint: 'wss://rpc.polkadot.io', db: 'db.sqlite' })
+await server.chain.newBlock()
 await server.close()
