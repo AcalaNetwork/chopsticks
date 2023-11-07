9791f708660d65086c6a5f65ffeac4b5887bb6db
Bryan Chen <xlchen1291@gmail.com>
2023-11-07 16:52:49 +1300

    ensure everything are JSON

---
+++ packages/chopsticks/src/plugins/run-block/index.test.ts
@@ -0,0 +1,31 @@
+import { describe, expect, it } from 'vitest'
+
+import { setup } from '@acala-network/chopsticks-core';
+
+import { rpc } from '.';
+
+describe('run-block', () => {
+  it('should work', async () => {
+    const chain = await setup({
+      endpoint: 'wss://rpc.polkadot.io',
+      block: 18000000,
+    })
+
+    const block = (await chain.getBlockAt(18000000 ))!
+    const header = await block.header
+    const parent = header.parentHash.toHex()
+
+    expect(await rpc({chain}, [{
+      includeRaw: true,
+      includeParsed: true,
+      includeBlockDetails: true,
+      parent,
+      block: {
+        header: header.toJSON(),
+        extrinsics: await block.extrinsics,
+      }
+    }])).toMatchSnapshot()
+
+    await chain.close()
+  }, 90000)
+})
