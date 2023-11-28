2a0f3bb733d8bf4755c76860b8a5904342aa82c1
Ermal Kaleci <ermalkaleci@gmail.com>
2023-11-28 12:45:15 +0100

    types alongside js files and publint

---
+++ publint.sh
@@ -0,0 +1,6 @@
+npx publint run --strict executor
+npx publint run --strict packages/core
+npx publint run --strict packages/db
+npx publint run --strict packages/chopsticks
+npx publint run --strict packages/testing
+npx publint run --strict packages/utils
