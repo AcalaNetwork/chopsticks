8059dbc5daf365dfbcfc1d1e87a6117d93516ab1
Xiliang Chen <xlchen1291@gmail.com>
2023-12-13 19:08:24 +1300

    fix decode cache (#592)

--- packages/chopsticks/src/plugins/run-block/index.test.ts
+++ packages/chopsticks/src/plugins/run-block/index.test.ts
@@ -32,4 +32,38 @@ describe('run-block', () => {
 
     await chain.close()
   }, 90000)
+
+  it('should work for acala', async () => {
+    const chain = await setup({
+      endpoint: 'wss://acala-rpc.aca-api.network',
+      block: 4851233,
+      db: !process.env.RUN_TESTS_WITHOUT_DB ? new SqliteDatabase('e2e-tests-db.sqlite') : undefined,
+    })
+
+    const parseBlock = async (n) => {
+      const block = (await chain.getBlockAt(n))!
+      const header = await block.header
+      const parent = header.parentHash.toHex()
+
+      const result = await rpc({ chain }, [
+        {
+          includeRaw: true,
+          includeParsed: true,
+          includeBlockDetails: true,
+          parent,
+          block: {
+            header: header.toJSON(),
+            extrinsics: await block.extrinsics,
+          },
+        },
+      ])
+      expect(result).toMatchSnapshot()
+    }
+
+    await parseBlock(4851229)
+    await parseBlock(4851230)
+    await parseBlock(4851231)
+
+    await chain.close()
+  }, 90000)
 })
