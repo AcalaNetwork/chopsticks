c82ac0a266bb83446b6aaba58a586b05741a03dd
Ermal Kaleci <ermalkaleci@gmail.com>
2024-01-16 17:01:55 +0100

    disable collectives test

--- packages/e2e/src/build-block.test.ts
+++ packages/e2e/src/build-block.test.ts
@@ -24,7 +24,7 @@ describe.runIf(process.env.CI).each([
 
   { chain: 'Westend', endpoint: 'wss://westend-rpc.polkadot.io' },
   { chain: 'Westmint', endpoint: 'wss://westmint-rpc.polkadot.io' },
-  { chain: 'Westend Collectives', endpoint: 'wss://sys.ibp.network/collectives-westend' },
+  // { chain: 'Westend Collectives', endpoint: 'wss://sys.ibp.network/collectives-westend' },
 ])('Latest $chain can build blocks', async ({ endpoint, storage }) => {
   const { setup, teardownAll } = await setupAll({ endpoint })
 
