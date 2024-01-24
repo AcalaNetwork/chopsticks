14c10bcdcbc0e5f06fc00f050c9739fc9eacae82
Ermal Kaleci <ermalkaleci@gmail.com>
2024-01-24 13:23:29 +0100

    update endpoints

--- packages/node-test/index.mjs
+++ packages/node-test/index.mjs
@@ -1,6 +1,9 @@
 import { setupWithServer, destroyWorker } from '@acala-network/chopsticks'
 
-const server = await setupWithServer({ endpoint: 'wss://rpc.polkadot.io', db: 'db.sqlite' })
+const server = await setupWithServer({
+	endpoint: ['wss://polkadot-rpc.dwellir.com', 'wss://rpc.ibp.network/polkadot', 'wss://rpc.polkadot.io'],
+	db: 'db.sqlite',
+})
 await server.chain.newBlock()
 await server.close()
 await destroyWorker()
