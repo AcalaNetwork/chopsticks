1b9da334dda73d7a658355a91c5c0c3c66b91cb3
Ermal Kaleci <ermalkaleci@gmail.com>
2024-03-11 13:05:46 +0100

    connectHorizontal open any channel

--- packages/core/src/xcm/horizontal.ts
+++ packages/core/src/xcm/horizontal.ts
@@ -30,5 +30,15 @@ export const connectHorizontal = async (parachains: Record<number, Blockchain>) 
         }
       }
     })
+
+    const hrmpHeads = await chain.head.read('BTreeMap<u32, H256>', meta.query.parachainSystem.lastHrmpMqcHeads)
+    if (hrmpHeads && !process.env.DISABLE_AUTO_HRMP) {
+      const existingChannels = Array.from(hrmpHeads.keys()).map((x) => x.toNumber())
+      for (const paraId in parachains) {
+        if (!existingChannels.includes(Number(paraId))) {
+          chain.submitHorizontalMessages(Number(paraId), [])
+        }
+      }
+    }
   }
 }
