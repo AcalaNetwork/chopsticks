ab991788d67a200e2ee8aee4f753a86bc49b3ad2
Ermal Kaleci <ermalkaleci@gmail.com>
2024-03-15 12:42:48 +0100

    fix grandpa subscription

--- packages/core/src/rpc/substrate/grandpa.ts
+++ packages/core/src/rpc/substrate/grandpa.ts
@@ -1,17 +1,20 @@
+import { blake2AsHex } from '@polkadot/util-crypto'
+
+import { Block } from '../../blockchain/block.js'
 import { Handler } from '../shared.js'
 
 export const grandpa_subscribeJustifications: Handler<void, string> = async (context, _params, { subscribe }) => {
-  let update = () => {}
+  let update = (_block: Block) => {}
 
-  const id = context.chain.headState.subscribeHead(() => update())
+  const id = context.chain.headState.subscribeHead((block) => update(block))
   const callback = subscribe('grandpa_justifications', id, () => context.chain.headState.unsubscribeHead(id))
 
-  update = async () => {
-    const mockProof = '0x' + '7'.repeat(64)
-    callback(mockProof)
+  update = async (block: Block) => {
+    // Proof needs to be different for each block
+    callback(blake2AsHex(block.hash))
   }
 
-  setTimeout(update, 50)
+  setTimeout(() => update(context.chain.head), 50)
 
   return id
 }
