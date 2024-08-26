f49ef1277f63e0a1d956f19a6df908e88dfb2423
Victor Oliva <olivarra1@gmail.com>
2024-08-26 12:10:31 +0200

    add body + function doc headers

--- packages/core/src/rpc/rpc-spec/chainHead_v1.ts
+++ packages/core/src/rpc/rpc-spec/chainHead_v1.ts
@@ -1,5 +1,5 @@
 import { Block } from '../../blockchain/block.js'
-import { Handler, SubscriptionManager } from '../shared.js'
+import { Handler, ResponseError, SubscriptionManager } from '../shared.js'
 import { HexString } from '@polkadot/util/types'
 import { defaultLogger } from '../../logger.js'
 
@@ -12,6 +12,15 @@ async function afterResponse(fn: () => void) {
   fn()
 }
 
+/**
+ * Start a chainHead follow subscription
+ *
+ * @param context
+ * @param params - [`withRuntime`]
+ * @param subscriptionManager
+ *
+ * @return subscription id
+ */
 export const chainHead_v1_follow: Handler<[boolean], string> = async (
   context,
   [withRuntime],
@@ -54,8 +63,8 @@ export const chainHead_v1_follow: Handler<[boolean], string> = async (
     context.chain.headState.unsubscribeHead(id)
     callbacks.delete(id)
   }
-  const callback = subscribe('chainHead_v1_followEvent', id, cleanup)
 
+  const callback = subscribe('chainHead_v1_followEvent', id, cleanup)
   callbacks.set(id, callback)
 
   afterResponse(async () => {
@@ -69,12 +78,27 @@ export const chainHead_v1_follow: Handler<[boolean], string> = async (
   return id
 }
 
+/**
+ * Stop a chainHead follow subscription
+ *
+ * @param context
+ * @param params - [`followSubscription`]
+ * @param subscriptionManager
+ */
 export const chainHead_v1_unfollow: Handler<[string], null> = async (_, [followSubscription], { unsubscribe }) => {
   unsubscribe(followSubscription)
 
   return null
 }
 
+/**
+ * Retrieve the header for a specific block
+ *
+ * @param context
+ * @param params - [`followSubscription`, `hash`]
+ *
+ * @return SCALE-encoded header, or null if the block is not found.
+ */
 export const chainHead_v1_header: Handler<[string, HexString], HexString | null> = async (
   context,
   [followSubscription, hash],
@@ -92,6 +116,14 @@ type OperationStarted = {
 const operationStarted = (operationId: string): OperationStarted => ({ result: 'started', operationId })
 const randomId = () => Math.random().toString(36).substring(2)
 
+/**
+ * Perform a runtime call for a block
+ *
+ * @param context
+ * @param params - [`followSubscription`, `hash`, `function`, `callParameters`]
+ *
+ * @return OperationStarted event with operationId to receive the result on the follow subscription
+ */
 export const chainHead_v1_call: Handler<[string, HexString, string, HexString], OperationStarted> = async (
   context,
   [followSubscription, hash, method, callParameters],
@@ -125,6 +157,15 @@ interface StorageItemRequest {
   key: HexString
   type: 'value' | 'hash' | 'closestDescendantMerkleValue' | 'descendantsValues' | 'descendantsHashes'
 }
+
+/**
+ * Query the storage for a given block
+ *
+ * @param context
+ * @param params - [`followSubscription`, `hash`, `items`, `childTrie`]
+ *
+ * @return OperationStarted event with operationId to receive the result on the follow subscription
+ */
 export const chainHead_v1_storage: Handler<
   [string, HexString, StorageItemRequest[], HexString | null],
   StorageStarted
@@ -201,4 +242,62 @@ export const chainHead_v1_storage: Handler<
     ...operationStarted(operationId),
     discardedItems: 0,
   }
+}
+
+type LimitReached = { result: 'limitReached' }
+const limitReached: LimitReached = { result: 'limitReached' }
+
+/**
+ * Retrieve the body of a specific block
+ *
+ * @param context
+ * @param params - [`followSubscription`, `hash`]
+ *
+ * @return OperationStarted event with operationId to receive the result on the follow subscription
+ */
+export const chainHead_v1_body: Handler<[string, HexString], OperationStarted | LimitReached> = async (
+  context,
+  [followSubscription, hash],
+) => {
+  if (!callbacks.has(followSubscription)) return limitReached
+  const block = await context.chain.getBlock(hash)
+  if (!block) {
+    throw new ResponseError(-32801, 'Block not found')
+  }
+
+  const operationId = randomId()
+  afterResponse(async () => {
+    const body = await block.extrinsics
+
+    callbacks.get(followSubscription)?.({
+      event: 'operationBodyDone',
+      operationId,
+      value: body,
+    })
+  })
+
+  return operationStarted(operationId)
+}
+
+// Currently no-ops, will come into play when pagination is implemented
+export const chainHead_v1_continue: Handler<[string, HexString], null> = async (
+  _context,
+  [_followSubscription, _operationId],
+) => {
+  return null
+}
+
+export const chainHead_v1_stopOperation: Handler<[string, HexString], null> = async (
+  _context,
+  [_followSubscription, _operationId],
+) => {
+  return null
+}
+
+// no-op, since there's no concept of unpinning in chopsticks
+export const chainHead_v1_unpin: Handler<[string, HexString | HexString[]], null> = async (
+  _context,
+  [_followSubscription, _hashOrHashes],
+) => {
+  return null
 }
