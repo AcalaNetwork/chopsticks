e647c5c4da65548c8633d932ee02a09306059931
Victor Oliva <olivarra1@gmail.com>
2024-09-09 15:39:58 +0200

    feat: add transaction_v1 group of functions

---
+++ packages/core/src/rpc/rpc-spec/transaction_v1.ts
@@ -0,0 +1,33 @@
+import { Handler } from '../shared.js'
+import { HexString } from '@polkadot/util/types'
+import { defaultLogger } from '@acala-network/chopsticks-core'
+
+const logger = defaultLogger.child({ name: 'rpc-transaction_v1' })
+const randomId = () => Math.random().toString(36).substring(2)
+
+/**
+ * Submit the extrinsic to the transaction pool
+ *
+ * @param context
+ * @param params - [`extrinsic`]
+ *
+ * @return operation id
+ */
+export const transaction_v1_broadcast: Handler<[HexString], string | null> = async (context, [extrinsic]) => {
+  await context.chain.submitExtrinsic(extrinsic).catch((err) => {
+    // As per the spec, the invalid transaction errors should be ignored.
+    logger.warn('Submit extrinsic failed', err)
+  })
+
+  return randomId()
+}
+
+/**
+ * Stop broadcasting the transaction to other nodes.
+ *
+ */
+export const transaction_v1_stop: Handler<[string], null> = async (_context, [_operationId]) => {
+  // Chopsticks doesn't have any process to broadcast the transaction through P2P
+  // so stopping doesn't have any effect.
+  return null
+}
---
+++ packages/e2e/src/rpc-spec.test.ts
@@ -0,0 +1,65 @@
+import { ApiPromise } from '@polkadot/api'
+import { describe, expect, it } from 'vitest'
+import { dev, env, observe, setupPolkadotApi, testingPairs } from './helper.js'
+
+const testApi = await setupPolkadotApi(env.acalaV15)
+
+const { alice, bob } = testingPairs()
+
+describe('transaction_v1', async () => {
+  it('sends and executes transactions', async () => {
+    const chainHead = testApi.observableClient.chainHead$()
+
+    const api = await prepareChainForTx()
+
+    const tx = await api.tx.balances.transferKeepAlive(bob.address, 100n).signAsync(alice)
+    const { nextValue, subscription } = observe(chainHead.trackTx$(tx.toHex()))
+    const resultPromise = nextValue()
+    const broadcast = testApi.observableClient.broadcastTx$(tx.toHex()).subscribe()
+
+    // We don't have a confirmation of when the transaction has been broadcasted through the network
+    // it just continues to get broadcasted through the nodes until we unsubscribe from it.
+    // In this case, where there's only one node, a couple of blocks should be enough.
+    await dev.newBlock()
+    const hash = await dev.newBlock()
+
+    expect(await resultPromise).toMatchObject({
+      hash,
+      found: {
+        type: true,
+      },
+    })
+
+    broadcast.unsubscribe()
+    subscription.unsubscribe()
+    chainHead.unfollow()
+  })
+})
+
+const UPGRADED = 0x80000000_00000000_00000000_00000000n
+const INITIAL_ACCOUNT_VALUE = 100_000_000_000_000n
+async function prepareChainForTx() {
+  const api = await ApiPromise.create({
+    provider: testApi.ws,
+    noInitWarn: true,
+  })
+  await api.isReady
+  await dev.setStorage({
+    System: {
+      Account: [
+        [
+          [alice.address],
+          {
+            data: { free: INITIAL_ACCOUNT_VALUE, flags: UPGRADED },
+          },
+        ],
+        [[bob.address], { data: { free: INITIAL_ACCOUNT_VALUE, flags: UPGRADED } }],
+      ],
+    },
+    Sudo: {
+      Key: alice.address,
+    },
+  })
+
+  return api
+}
