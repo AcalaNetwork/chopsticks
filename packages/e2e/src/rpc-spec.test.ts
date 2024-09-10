9c6189d85fb289ec0c122b3605c6d486223a28ec
Victor Oliva <olivarra1@gmail.com>
2024-09-10 09:40:59 +0200

    change upgraded flag for providers

--- packages/e2e/src/rpc-spec.test.ts
+++ packages/e2e/src/rpc-spec.test.ts
@@ -1,6 +1,8 @@
 import { ApiPromise } from '@polkadot/api'
+import { RuntimeContext } from '@polkadot-api/observable-client'
 import { describe, expect, it } from 'vitest'
 import { dev, env, observe, setupPolkadotApi, testingPairs } from './helper.js'
+import { firstValueFrom } from 'rxjs'
 
 const testApi = await setupPolkadotApi(env.acalaV15)
 
@@ -12,15 +14,16 @@ describe('transaction_v1', async () => {
 
     const api = await prepareChainForTx()
 
-    const tx = await api.tx.balances.transferKeepAlive(bob.address, 100n).signAsync(alice)
+    const TRANSFERRED_VALUE = 100n
+    const tx = await api.tx.balances.transferKeepAlive(bob.address, TRANSFERRED_VALUE).signAsync(alice)
     const { nextValue, subscription } = observe(chainHead.trackTx$(tx.toHex()))
     const resultPromise = nextValue()
     const broadcast = testApi.observableClient.broadcastTx$(tx.toHex()).subscribe()
 
     // We don't have a confirmation of when the transaction has been broadcasted through the network
     // it just continues to get broadcasted through the nodes until we unsubscribe from it.
-    // In this case, where there's only one node, waiting for 300ms should be enough.
-    await new Promise((resolve) => setTimeout(resolve, 300))
+    // In this case, where there's only one node, waiting for 500ms should be enough.
+    await new Promise((resolve) => setTimeout(resolve, 500))
     const hash = await dev.newBlock()
 
     expect(await resultPromise).toMatchObject({
@@ -30,13 +33,24 @@ describe('transaction_v1', async () => {
       },
     })
 
+    const keyEncoder = (addr: string) => (ctx: RuntimeContext) =>
+      ctx.dynamicBuilder.buildStorage('System', 'Account').enc(addr)
+    const resultDecoder = (data: string | null, ctx: RuntimeContext) =>
+      data ? ctx.dynamicBuilder.buildStorage('System', 'Account').dec(data) : null
+    expect(
+      await firstValueFrom(chainHead.storage$(null, 'value', keyEncoder(bob.address), null, resultDecoder)),
+    ).toMatchObject({
+      data: {
+        free: INITIAL_ACCOUNT_VALUE + TRANSFERRED_VALUE,
+      },
+    })
+
     broadcast.unsubscribe()
     subscription.unsubscribe()
     chainHead.unfollow()
   })
 })
 
-const UPGRADED = 0x80000000_00000000_00000000_00000000n
 const INITIAL_ACCOUNT_VALUE = 100_000_000_000_000n
 async function prepareChainForTx() {
   const api = await ApiPromise.create({
@@ -50,10 +64,17 @@ async function prepareChainForTx() {
         [
           [alice.address],
           {
-            data: { free: INITIAL_ACCOUNT_VALUE, flags: UPGRADED },
+            providers: 1,
+            data: { free: INITIAL_ACCOUNT_VALUE },
           },
         ],
-        [[bob.address], { data: { free: INITIAL_ACCOUNT_VALUE, flags: UPGRADED } }],
+        [
+          [bob.address],
+          {
+            providers: 1,
+            data: { free: INITIAL_ACCOUNT_VALUE },
+          },
+        ],
       ],
     },
     Sudo: {
