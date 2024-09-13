e43faade56a8b83aada082a6eaeca73c1cd3207a
Victor Oliva <olivarra1@gmail.com>
2024-09-13 10:10:57 +0200

    throw error if genesis is null

--- packages/core/src/rpc/rpc-spec/chainSpec_v1.ts
+++ packages/core/src/rpc/rpc-spec/chainSpec_v1.ts
@@ -1,12 +1,17 @@
 import { ChainProperties } from '../../index.js'
-import { Handler } from '../shared.js'
+import { Handler, ResponseError } from '../shared.js'
+import { HexString } from '@polkadot/util/types'
 
 export const chainSpec_v1_chainName: Handler<[], string> = async (context) => {
   return context.chain.api.getSystemChain()
 }
 
-export const chainSpec_v1_genesisHash: Handler<[], string> = async (context) => {
-  return (await context.chain.api.getBlockHash(0)) ?? ''
+export const chainSpec_v1_genesisHash: Handler<[], HexString> = async (context) => {
+  const genesisHash = await context.chain.api.getBlockHash(0)
+  if (genesisHash === null) {
+    throw new ResponseError(1, 'Unexpected null genesis hash')
+  }
+  return genesisHash
 }
 
 export const chainSpec_v1_properties: Handler<[], ChainProperties> = async (context) => {
