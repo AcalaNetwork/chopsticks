828492537e7a78ea3007126b9ca02c3c2036b7ec
girazoki <gorka.irazoki@gmail.com>
2024-10-08 11:10:14 +0200

    pr suggestions

--- packages/core/src/blockchain/inherent/parachain/nimbus-author-inherent.ts
+++ packages/core/src/blockchain/inherent/parachain/nimbus-author-inherent.ts
@@ -12,19 +12,20 @@ export class SetNimbusAuthorInherent implements InherentProvider {
 
     const meta = await parent.meta
 
-    // mock author inherent data and authorities noting data
-    const layer = newBlock.pushStorageLayer()
-
-    if (meta.query.authorNoting) {
-      layer.set(
-        compactHex(meta.query.authorNoting.didSetContainerAuthorData()),
-        meta.registry.createType('bool', true).toHex(),
-      )
-    }
-
     if (!meta.tx.authorInherent?.kickOffAuthorshipValidation) {
+      if (meta.query.authorNoting) {
+        newBlock
+          .pushStorageLayer()
+          .set(
+            compactHex(meta.query.authorNoting.didSetContainerAuthorData()),
+            meta.registry.createType('bool', true).toHex(),
+          )
+      }
       return []
     }
+
+    // mock author inherent data and authorities noting data
+    const layer = newBlock.pushStorageLayer()
 
     const accountType = meta.registry.hasType('NimbusPrimitivesNimbusCryptoPublic')
       ? 'NimbusPrimitivesNimbusCryptoPublic'
@@ -72,6 +73,10 @@ export class SetNimbusAuthorInherent implements InherentProvider {
             .toHex(),
         )
       }
+      layer.set(
+        compactHex(meta.query.authorNoting.didSetContainerAuthorData()),
+        meta.registry.createType('bool', true).toHex(),
+      )
     }
     return [new GenericExtrinsic(meta.registry, meta.tx.authorInherent.kickOffAuthorshipValidation()).toHex()]
   }
