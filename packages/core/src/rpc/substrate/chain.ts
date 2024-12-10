7b962a0e6da05d3ab62de187dee03b875db9a837
Thibaut Sardan <github@thib.top>
2024-12-10 18:35:41 +0100

    support archive_unstable_body

--- packages/core/src/rpc/substrate/chain.ts
+++ packages/core/src/rpc/substrate/chain.ts
@@ -73,6 +73,20 @@ export const chain_getBlock: Handler<
 
 /**
  * @param context
+ * @param params - [`blockhash`]
+ *
+ * @return Block extrinsics
+ */
+export const archive_unstable_body: Handler<[HexString], HexString[]> = async (context, [hash]) => {
+  const block = await context.chain.getBlock(hash)
+  if (!block) {
+    throw new ResponseError(1, `Block ${hash} not found`)
+  }
+  return await block.extrinsics
+}
+
+/**
+ * @param context
  *
  * @return head hash
  */
