b1615156d35d4a23b71e5561dcc88e6b0a7fbd12
Alexandre Baldé <alexandre.balde@parity.io>
2025-01-16 14:36:15 +0000

    Export PAPI test signers from `chopstick-utils`

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -15,6 +15,13 @@ import type { SubmittableExtrinsic } from '@polkadot/api-base/types'
 import { Keyring, createTestKeyring } from '@polkadot/keyring'
 import type { Codec } from '@polkadot/types/types'
 import type { HexString } from '@polkadot/util/types'
+import { ed25519CreateDerive, sr25519CreateDerive } from "@polkadot-labs/hdkd"
+import {
+  DEV_PHRASE,
+  entropyToMiniSecret,
+  mnemonicToEntropy,
+} from "@polkadot-labs/hdkd-helpers"
+import { getPolkadotSigner } from "polkadot-api/signer"
 
 const logger = defaultLogger.child({ name: 'utils' })
 
@@ -273,5 +280,42 @@ export const testingPairs = (keyringType: 'ed25519' | 'sr25519' = 'ed25519', ss5
 
     keyring,
     keyringEth,
+  }
+}
+
+/**
+ * Creates a set of test signers for either Ed25519 or Sr25519.
+ * @param keyringType Type of keyring to use for substrate addresses ('ed25519' or 'sr25519')
+ * @returns Object containing various test signers, and a `polkadotSignerBuilder` function to create more.
+ */
+export const testingSigners = (keyringType: 'Ed25519' | 'Sr25519' = 'Ed25519') => {
+  const entropy = mnemonicToEntropy(DEV_PHRASE)
+  const miniSecret = entropyToMiniSecret(entropy)
+  const derive = keyringType === 'Ed25519'
+    ? ed25519CreateDerive(miniSecret)
+    : sr25519CreateDerive(miniSecret);
+
+  // Create a PAPI polkadot signer given a derivation path e.g. "//Alice" or "//Bob".
+  const polkadotSignerBuilder = (
+    keyringType: 'Ed25519' | 'Sr25519' = 'Ed25519',
+    path: string
+  ) => {
+    const hdkdKeyPair = derive(path)
+    return getPolkadotSigner(
+      hdkdKeyPair.publicKey,
+      keyringType,
+      hdkdKeyPair.sign,
+    )
+  }
+
+  return {
+    alice: polkadotSignerBuilder(keyringType, '//Alice'),
+    bob: polkadotSignerBuilder(keyringType, '//Bob'),
+    charlie: polkadotSignerBuilder(keyringType, '//Charlie'),
+    dave: polkadotSignerBuilder(keyringType, '//Dave'),
+    eve: polkadotSignerBuilder(keyringType, '//Eve'),
+    ferdie: polkadotSignerBuilder(keyringType, '//Ferdie'),
+
+    polkadotSignerBuilder
   }
 }
