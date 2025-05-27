0693f650a5c7c4ff9901321ab48e6c50cadb3ee7
Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>
2025-05-27 23:02:27 +0200

    fix archive call

--- packages/core/src/rpc/substrate/archive.ts
+++ packages/core/src/rpc/substrate/archive.ts
@@ -2,7 +2,6 @@ import type { HexString } from '@polkadot/util/types'
 
 import { type Handler, ResponseError } from '../shared.js'
 import { chain_getBlockHash } from './chain.js'
-import { state_call } from './state.js'
 
 /**
  * @param context
@@ -18,5 +17,23 @@ export const archive_unstable_body: Handler<[HexString], HexString[]> = async (c
   return await block.extrinsics
 }
 
+/**
+ * @param context
+ * @param params - [`blockhash`, `method`, `data` ]
+*
+* @return result in hash
+*/
+export const archive_unstable_call: Handler<[HexString, string, HexString], { success: boolean; value: `0x${string}`; }> = async (
+  context,
+  [hash, method, data],
+) => {
+  const block = await context.chain.getBlock(hash)
+  if (!block) {
+    throw new ResponseError(1, `Block ${hash} not found`)
+  }
+  
+  const resp = await block.call(method, [data])
+  return {success: true, value: resp.result}
+}
+
 export const archive_unstable_hashByHeight = chain_getBlockHash
-export const archive_unstable_call = state_call
