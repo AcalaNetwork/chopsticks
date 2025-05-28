fd1aa1961fd7d30e88e55d61f763e7b1fb2a9cec
Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>
2025-05-28 10:59:20 +0200

    fix lint and jsdocs

--- packages/core/src/rpc/substrate/archive.ts
+++ packages/core/src/rpc/substrate/archive.ts
@@ -20,20 +20,22 @@ export const archive_unstable_body: Handler<[HexString], HexString[]> = async (c
 /**
  * @param context
  * @param params - [`blockhash`, `method`, `data` ]
-*
-* @return result in hash
-*/
-export const archive_unstable_call: Handler<[HexString, string, HexString], { success: boolean; value: `0x${string}`; }> = async (
-  context,
-  [hash, method, data],
-) => {
+ *
+ * @return {Object} result The call result.
+ * @return {boolean} result.success Whether the call is successful.
+ * @return {string} result.value The call result.
+ */
+export const archive_unstable_call: Handler<
+  [HexString, string, HexString],
+  { success: boolean; value: `0x${string}` }
+> = async (context, [hash, method, data]) => {
   const block = await context.chain.getBlock(hash)
   if (!block) {
     throw new ResponseError(1, `Block ${hash} not found`)
   }
-  
+
   const resp = await block.call(method, [data])
-  return {success: true, value: resp.result}
+  return { success: true, value: resp.result }
 }
 
 export const archive_unstable_hashByHeight = chain_getBlockHash
