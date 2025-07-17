8ec39cb456047ea91897438e3d6b39eb8a215d15
Bryan Chen <xlchen1291@gmail.com>
2025-07-17 14:03:41 +1200

    Add release script, update devDependencies with glob/semver, and extend editorconfig for mjs

--- packages/node-test/index.mjs
+++ packages/node-test/index.mjs
@@ -1,8 +1,8 @@
 import { destroyWorker, setupWithServer } from '@acala-network/chopsticks'
 
 const server = await setupWithServer({
-	endpoint: ['wss://polkadot-rpc.dwellir.com', 'wss://rpc.ibp.network/polkadot', 'wss://rpc.polkadot.io'],
-	db: 'db.sqlite',
+  endpoint: ['wss://polkadot-rpc.dwellir.com', 'wss://rpc.ibp.network/polkadot', 'wss://rpc.polkadot.io'],
+  db: 'db.sqlite',
 })
 await server.chain.newBlock()
 await server.close()
